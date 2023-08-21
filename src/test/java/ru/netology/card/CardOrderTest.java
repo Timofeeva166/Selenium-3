fab8e2597e77d9b8b24f01d718c9903a18662018
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-08-21 21:23:52 +0300

    Negative tests added

--- src/test/java/ru/netology/card/CardOrderTest.java
+++ src/test/java/ru/netology/card/CardOrderTest.java
@@ -4,8 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -39,11 +40,7 @@ public class CardOrderTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-          "Дмитрий, +79999999999, Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
-          "Иванов Иван, +68355672431, Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
-          "Кузнецова Анна-Мария, +28888888888, Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
-    })
+    @CsvFileSource(resources = "/valid.csv")
     public void shouldGetValidResults(String name, String phone, String expected) { //форма заполняется валидными данными
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys(name);
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys(phone);
@@ -53,6 +50,70 @@ public class CardOrderTest {
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
 
         assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalidForName.csv", delimiter = '|')
+    public  void shouldNotGetInvalidResultsForName(String name, String phone, String expected) { //невалидные значения для имени
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys(name);
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys(phone);
+        driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
+        driver.findElement(By.className("button")).click();
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalidForPhone.csv", delimiter = '|')
+    public  void shouldNotGetInvalidResultsForPhone(String name, String phone, String expected) { //невалидные значения для телефона
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys(name);
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys(phone);
+        driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
+        driver.findElement(By.className("button")).click();
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldNotGetWithoutName() { //поле "имя" не заполнено
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
+        driver.findElement(By.className("button")).click();
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+
+        assertEquals("Поле обязательно для заполнения", actual);
+
+    }
+
+    @Test
+    public void shouldNotGetWithoutPhone() { //поле "телефон" не заполнено
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Белов Ярослав");
+        driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
+        driver.findElement(By.className("button")).click();
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+
+        assertEquals("Поле обязательно для заполнения", actual);
+
+    }
+
+    @Test
+    public void shouldNotGetWithoutAccept() { //не поставлена галочка
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Белов Ярослав");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.className("button")).click();
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText().trim();
+
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", actual);
+
     }
 
 }
